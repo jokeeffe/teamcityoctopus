2a413ebf783962cc582ceea15506a0601ace1bd7
James O'Keeffe <james.okeeffe@bulletproof.net>
2017-03-01 10:48:53 +1100

    Changed contact

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace WebApplication1.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "James O'Keeffe.";
 
             return View();
         }
