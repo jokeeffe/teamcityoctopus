84465f530767150b37be3476b654bfd608df8d24
James O'Keeffe <james.okeeffe@bulletproof.net>
2017-03-06 18:04:34 +1100

    v2

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace WebApplication1.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "James A. (Iceman) O'Keeffe. v1";
+            ViewBag.Message = "James A. (Iceman) O'Keeffe. v2";
 
             return View();
         }
